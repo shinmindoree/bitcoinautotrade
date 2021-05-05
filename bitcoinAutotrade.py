641e313d7da2a02a866b2c26f5101cab787a01d5
shinmindoree <43820715+shinmindoree@users.noreply.github.com>
2021-05-05 22:45:16 +0900

    Update bitcoinAutotrade.py

--- bitcoinAutotrade.py
+++ bitcoinAutotrade.py
@@ -39,20 +39,20 @@ print("autotrade start")
 while True:
     try:
         now = datetime.datetime.now()
-        start_time = get_start_time("KRW-BTC")
+        start_time = get_start_time("KRW-DOGE")
         end_time = start_time + datetime.timedelta(days=1)
 
         if start_time < now < end_time - datetime.timedelta(seconds=10):
-            target_price = get_target_price("KRW-BTC", 0.5)
-            current_price = get_current_price("KRW-BTC")
+            target_price = get_target_price("KRW-DOGE", 0.5)
+            current_price = get_current_price("KRW-DOGE")
             if target_price < current_price:
                 krw = get_balance("KRW")
                 if krw > 5000:
-                    upbit.buy_market_order("KRW-BTC", krw*0.9995)
+                    upbit.buy_market_order("KRW-DOGE", krw*0.9995)
         else:
-            btc = get_balance("BTC")
-            if btc > 0.00008:
-                upbit.sell_market_order("KRW-BTC", btc*0.9995)
+            doge = get_balance("DOGE")
+            if doge > 0.00008:
+                upbit.sell_market_order("KRW-DOGE", doge*0.9995)
         time.sleep(1)
     except Exception as e:
         print(e)
